dfff0aee2d0cba14b425e22fd2481dcccdb2c071
glen <glen_dovey@hotmail.com>
2022-12-14 19:43:09 +0000

    end of interview style video

--- src/convertRomanNumerals.js
+++ src/convertRomanNumerals.js
@@ -1,3 +1,36 @@
 export const convertRomanNumerals = (romanNumerals) => {
+    let currentValue = 0
+    for (let i = 0; i < romanNumerals.length; i++) {
+        romanNumerals[i + 1]
+        currentValue += convertSingleRomanNumeral(romanNumerals[i])
+    }
+    return currentValue
+}
+
+const convertSingleRomanNumeral = (romanNumeral) => {
+    switch (romanNumeral) {
+        case "I":
+            return 1;
+        case "V":
+            return 5;
+        case "X":
+            return 10;
+        case "L":
+            return 50;
+        case "C":
+            return 100;
+        case "D":
+            return 500;
+        case "M":
+            return 1000;
+    }
     return undefined
 }
+
+// I = 1
+// V = 5
+// X = 10
+// L = 50
+// C = 100
+// D = 500
+// M = 1000
--- src/convertRomanNumerals.test.js
+++ src/convertRomanNumerals.test.js
@@ -1,7 +1,36 @@
 import { convertRomanNumerals } from "./convertRomanNumerals";
 
 describe("convertRomanNumerals", () => {
+    // test that everything comes in uppercase.
+    // input should conform the necessary requirements.
     it("Should convert I to 1", () => {
         expect(convertRomanNumerals("I")).toEqual(1)
+    })
+    it("Should convert V to 5", () => {
+        expect(convertRomanNumerals("V")).toEqual(5)
+    })
+    it("Should convert X to 10", () => {
+        expect(convertRomanNumerals("X")).toEqual(10)
+    })
+    it("Should convert L to 50", () => {
+        expect(convertRomanNumerals("L")).toEqual(50)
+    })
+    it("Should convert D to 500", () => {
+        expect(convertRomanNumerals("D")).toEqual(500)
+    })
+    it("Should convert M to 1000", () => {
+        expect(convertRomanNumerals("M")).toEqual(1000)
+    })
+    it("Should convert VI to 6", () => {
+        expect(convertRomanNumerals("VI")).toEqual(6)
+    })
+    it("Should convert IIII to 4", () => {
+        expect(convertRomanNumerals("IIII")).toEqual(4)
+    })
+    it("Should convert DCLX to 660", () => {
+        expect(convertRomanNumerals("DCLX")).toEqual(660)
+    })
+    it("Should convert IV to 4", () => {
+        expect(convertRomanNumerals("IV")).toEqual(4)
     })
 })
